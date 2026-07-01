4fefe87d77b2e26026acef074501c000c55c8352
ddandu <devendra2402@gmail.com>
2026-07-01 10:05:13 +0530

    second code

--- Hello-word.sh
+++ Hello-word.sh
@@ -2,7 +2,9 @@
 
 # This first program to leran shell scripting 
 
-Name1=Devendra
-Name2=Reshma
+Name1=$1
+Name2=$2
+
 echo "Hi $Name1 :: how are you"
 echo "I'm good Hi $Name2 how are you"
+echo "I'm good how ae you $Name1 "
