24abf80092eea24ff905193c1855be029c74d107
ddandu <devendra2402@gmail.com>
2026-07-02 10:22:39 +0530

    condtion

---
+++ Conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ] ; then 
+    echo "The given number is $NUMBER less then 10"
+else
+   echo "The given number is $NUMBER greather then 10"
+
+   fi
