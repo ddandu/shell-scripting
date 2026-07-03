cf4237f3afcff518b05ef40d9d250cf245a1e8b6
ddandu <devendra2402@gmail.com>
2026-07-03 11:45:06 +0530

    added

---
+++ logs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script start execued at : $(date)"
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root priviiages"
+    exit 1 # Failure is other then 0
+fi
+
+# Call the validate function
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+    echo -e "Installing $2 ...$R FAILURE $N"
+    exit 1
+ else
+    echo -e "Installing $2...$G is success $N"
+ fi
+
+} 
+  
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]; then 
+   dnf install mysql -y  &>>$LOG_FILE
+   VALIDATE $? "mysql"
+else
+   echo -e "MySql is already exit...$Y SKIPPED $N"
+fi
+
+dnf list installed nginx  &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   dnf install nginx -y  &>>$LOG_FILE
+   VALIDATE $? "nginx"
+else
+   echo -e "Nginx id already exit.... $Y SKIPPEd $N"
+fi 
