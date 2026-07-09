4436106a275e5492623f9d554636bb054075c755
ddandu <devendra2402@gmail.com>
2026-07-09 19:17:17 +0530

    disk usage

--- delete-old-files.sh
+++ delete-old-files.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # USERID=$(id -u)
 # R="\e[31m"
@@ -15,16 +15,16 @@
 echo "Script started executed at: $(date)" 
 SOURCE_DIR=/root/dev/app-log
 
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d $SOURCE_DIR ]; then   #if [ ! -d $SOURCE_DIR ] → Tests if the directory does not exist (-d checks for directory).
     echo -e "ERROR:: $SOURCE_DIR does not exist"
     exit 1
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 
-while IFS= read -r filepath   #Internal filed seperattor IFS
+while IFS= read -r filepath   #IFS= → Clears the Internal Field Separator so filenames with spaces are handled correctly.
 do
     echo "Deleting the file: $filepath"
     rm -rf $filepath
     echo "Deleted the file: $filepath"
-done <<< $FILES_TO_DELETE
+done <<< $FILES_TO_DELETE   #<<< $FILES_TO_DELETE → Feeds the contents of FILES_TO_DELETE into the loop.
